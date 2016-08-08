d317ef940efc650d76d9875971f84bec7443afc1
Christian Drüke <cdrueke@gmx.de>
2016-08-08 19:51:59 +0200

    load job configs into job list within the jobmanager

--- jobmanager.py
+++ jobmanager.py
@@ -1,7 +1,11 @@
-from bot import NotificationBot
-from user import User        
 import logging
 import logging.config    
+import glob
+import json
+
+from bot import NotificationBot
+from user import User  
+from job import Job
 
 class JobManager():
     """description of class"""
@@ -19,6 +23,15 @@ class JobManager():
             self.logger.info('{0}'.format(user_config[0]))
             self.users.append(User(user_config))
         
+        # init jobs
+        self.logger.info("Initialize jobs:")  
+        self.job_path = config['job_folder']
+        for job_config in glob.iglob(self.job_path + '/*.job.json', recursive=True):
+            with open(job_config) as fp:
+                job_config = json.load(fp)                
+                self.logger.info('{0}'.format(job_config['name']))
+                self.jobs.append(Job(job_config))
+
         # init notification bot
         self.logger.info("Initialize notification bot")   
         self.bot = NotificationBot(self, self.users, self.cfg['bot_token'])
